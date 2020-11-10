ba353f67decae7efd336fce84f0830a2d202d87e
ITSFlopItTimeYT <44099468+ITSFlopItTimeYT@users.noreply.github.com>
2020-11-10 08:41:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ app.get('/', (req, res) => res.send('Uptimebot!'));
 app.listen(port, () => console.log(`Example app listening at com.sun.net.httpserver.HttpServer:${port}`));
 //bot-code
 
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log('The client is ready!')
+ client.user.setPresence({
+      activity: {
+        name: "Finished Tutorial",
+        type: "WATCHING",
+      },
+    })
+})
+
+
+//end-code
+client.login(process.env.token)
